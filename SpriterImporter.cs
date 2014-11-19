791cf361e98b0513f2aa67d86dbe30eee33b922b
siquel <squual@kahvipaussi.net>
2014-11-19 18:19:07 +0200

    Initial commit

---
+++ SpriterImporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace SaNi.Spriter.Pipeline
+{
+    [ContentImporter(".scml", DisplayName = "SaNi.Spriter SCML - Spriter importer", DefaultProcessor = "SpriterProcessor")]
+    public class SpriterImporter : ContentImporter<XDocument>
+    {
+        public override XDocument Import(string filename, ContentImporterContext context)
+        {
+            return new XDocument();
+        }
+    }
+}
---
+++ SpriterWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace SaNi.Spriter.Pipeline
+{
+    [ContentTypeWriter]
+    public class SpriterWriter : ContentTypeWriter<SpriterShadowData>
+    {
+        /// <summary>
+        /// ÄLÄ KOSKAAN MENE KOSKEMAAN TÄHÄN, ÄLÄ KOSKAAN
+        /// Osottaa suoraan spriter API:n .dll, jos vaihdat, vaihda SaNi.Spriterin assembly nimi 
+        /// </summary>
+        /// <param name="targetPlatform"></param>
+        /// <returns></returns>
+        public override string GetRuntimeType(TargetPlatform targetPlatform)
+        {
+            return "SaNi.Spriter.SpriterModel, SaNi.Spriter";
+        }
+
+        /// <summary>
+        /// ÄLÄ KOSKAAN MENE KOSKEMAAN TÄHÄN, ÄLÄ KOSKAAN
+        /// Osottaa suoraan spriter API:n .dll, jos vaihdat, vaihda SaNi.Spriterin assembly nimi 
+        /// </summary>
+        /// <param name="targetPlatform"></param>
+        /// <returns></returns>
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return "SaNi.Spriter.SpriterReader, SaNi.Spriter";
+        }
+
+        protected override void Write(ContentWriter output, SpriterShadowData value)
+        {
+            output.Write(0);
+        }
+    }
+}
